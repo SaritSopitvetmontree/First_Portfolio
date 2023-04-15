3fe1804d7050333b8429f78634dc3f03cc28d4a9
Sarit Sopitvetmontree <sarit.sopitvetmontree@gmail.com>
2023-04-15 21:07:48 +0900

    Update cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import { BrowserRouter } from "react-router-dom";
+import AnimatedCursor from "react-animated-cursor"
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <AnimatedCursor
+      innerSize={16}
+      outerSize={16}
+      color='145, 94, 255'
+      outerAlpha={0.4}
+      innerScale={0.7}
+      outerScale={5}
+      clickables={[
+        'a',
+        'input[type="text"]',
+        'input[type="email"]',
+        'input[type="number"]',
+        'input[type="submit"]',
+        'input[type="image"]',
+        'label[for]',
+        'select',
+        'textarea',
+        'button',
+        '.link'
+      ]}
+      />
+
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
