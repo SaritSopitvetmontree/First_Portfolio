3945877841fc7261f64348b0594f9b7f48911374
Sarit Sopitvetmontree <sarit.sopitvetmontree@gmail.com>
2023-04-15 15:34:18 +0900

    Update Experience

--- src/assets/index.js
+++ src/assets/index.js
@@ -23,8 +23,8 @@ import threejs from "./tech/threejs.svg";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import Liigo from "./company/Liigo.png";
+import KELPS from "./company/KELPS.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -54,8 +54,8 @@ export {
   threejs,
   meta,
   shopify,
-  starbucks,
-  tesla,
+  Liigo,
+  KELPS,
   carrent,
   jobit,
   tripguide,
